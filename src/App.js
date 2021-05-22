24aedfdbf03ff2ea9946a8549265ad161ae66a2b
cse-mkamble <76428798+cse-mkamble@users.noreply.github.com>
2021-05-22 18:48:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./containers/Home";
+import Signin from "./containers/Signin";
+import Signup from "./containers/Signup";
 import './App.css';
+import PrivateRoute from "./components/HOC/PrivateRoute";
+import { useDispatch, useSelector } from "react-redux";
+import { isUserLoggedIn, getAllCategory, getInitialData } from "./actions";
+import Orders from './containers/Orders';
+import Products from './containers/Products';
+import Category from './containers/Category';
+import NewPage from './containers/NewPage';
 
-function App() {
+const App = (props) => {
+
+  const dispatch = useDispatch();
+  const auth = useSelector(state => state.auth);
+
+  useEffect(() => {
+    if (!auth.authenticate) {
+      dispatch(isUserLoggedIn())
+    }
+    if (auth.authenticate) {
+      dispatch(getInitialData())
+    }
+  }, [auth.authenticate])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Switch>
+        <PrivateRoute path='/' exact component={Home} />
+        <PrivateRoute path='/page' component={NewPage} />
+        <PrivateRoute path='/category' component={Category} />
+        <PrivateRoute path='/products' component={Products} />
+        <PrivateRoute path='/orders' component={Orders} />
+
+        <Route path='/Signin' component={Signin} />
+        <Route path='/Signup' component={Signup} />
+      </Switch>
     </div>
   );
 }
---
+++ src/urlConfig.js
@@ -0,0 +1,4 @@
+export const api = 'http://65.0.9.242:4000/api'
+export const generatePublicUrl = (fileName) => {
+    return `http://65.0.9.242:4000/public/${fileName}`
+}
